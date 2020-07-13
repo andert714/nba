c4f990635d03b18cd9871be15a82410dfedc489d
Travis Andersen <andert714@gmail.com>
2020-07-13 15:33:18 -0600

    Added team name lists

---
+++ meta.py
@@ -0,0 +1,18 @@
+########## NBA Teams ##########
+nba_teams = ['ATL', 'BOS', 'BRK', 'CHI', 'CHO', 'CLE',
+             'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND',
+             'LAC', 'LAL', 'MEM', 'MIA', 'MIL', 'MIN',
+             'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO',
+             'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
+
+########## NBA Team Names ##########
+nba_team_names = ['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets',
+       'Chicago Bulls', 'Charlotte Hornets', 'Cleveland Cavaliers',
+       'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons',
+       'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers',
+       'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies',
+       'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves',
+       'New Orleans Pelicans', 'New York Knicks', 'Oklahoma City Thunder',
+       'Orlando Magic', 'Philadelphia 76ers', 'Phoenix Suns',
+       'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs',
+       'Toronto Raptors', 'Utah Jazz', 'Washington Wizards']
