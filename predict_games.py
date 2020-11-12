f8c00c64a09f30528a3efefd17c4409e892542a5
Travis Andersen <andert714@gmail.com>
2020-11-12 13:11:24 -0700

    Cross validation for best alpha, got 0.67 accuracy on validation set

--- predict_games.py
+++ predict_games.py
@@ -3,7 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import web_scraping
 import meta
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.linear_model import LogisticRegression
+
+# Scrape data from basketball-reference.com
+# for year in range(2015, 2020):
+#    filepath = 'data/schedules/{}.csv'.format(year)
+#    schedule = web_scraping.scrape_schedule(year)
+#    schedule.to_csv(filepath)
 
 def clean_schedule(df, year):
     df['season'] = year
@@ -18,13 +25,6 @@ def clean_schedule(df, year):
 
     return df
 
-# Scrape data from basketball-reference.com
-# for year in range(2015, 2020):
-#    filepath = 'data/schedules/{}.csv'.format(year)
-#    schedule = web_scraping.scrape_schedule(year)
-#    schedule.to_csv(filepath)
-
-
 # Combine data from 2014 to 2019
 df = pd.DataFrame()
 for year in range(2015, 2020):
@@ -33,21 +33,58 @@ for year in range(2015, 2020):
     schedule = clean_schedule(schedule, year)
     df = df.append(schedule)
 
-# Create y-vector of home margins
-df['margin'] = df['margin']*(2*df['home'] - 1)
-y = np.array(df.query('home == 1')['margin'] > 0, 'int64')
+def get_features(df, alpha):
+    # Create y-vector of home margins
+    df = df.copy()
+    y = np.array(df.query('home == 1')['margin'] > 0, 'int64')
 
-# Create exponentially weighted margins grouped by team
-df['margin'] = df.groupby('team')['margin'].transform(lambda x: x.ewm(alpha=0.05).mean().shift(1))
-df = df.pivot(index=['season','game_id'], columns='home', values=['margin','team'])
+    # Create exponentially weighted margins grouped by team
+    df['margin'] = df['margin']*(2*df['home'] - 1)
+    df['margin'] = df.groupby('team')['margin'].transform(lambda x: x.ewm(alpha=alpha).mean().shift(1))
+    df = df.pivot(index=['season','game_id'], columns='home', values=['margin','team'])
 
-# Create X matrix (column of difference in exponentially weighted margins)
-X = np.array(df['margin'][1] - df['margin'][0], 'float64').reshape(-1,1)
+    # Create X matrix (column of difference in exponentially weighted margins)
+    X = np.array(df['margin'][1] - df['margin'][0], 'float64').reshape(-1,1)
 
-# Take out first 100 games
-X = X[100:]
-y = y[100:]
+    # Take out first 100 games
+    X = X[100:]
+    y = y[100:]
+    return X, y
 
 # Model specification
 # X = [1 (home) | (home exponentially weighted margin - away exponentially weighted margin)_i]
 # y = [margin_i]
+
+# Cross validation for tuning alpha parameter
+alphas = np.linspace(1e-5, 0.2, 100)
+scores = np.zeros(len(alphas))
+
+for i in range(len(alphas)):
+    X, y = get_features(df, alphas[i])
+    X = X[:-1000]
+    y = y[:-1000]
+    cv = TimeSeriesSplit()
+    cv_scores = []
+    for train_index, test_index in cv.split(X):
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        m = LogisticRegression(penalty='none').fit(X_train, y_train)
+        cv_scores.append(m.score(X_test, y_test))
+
+    scores[i] = np.mean(cv_scores)
+
+plt.plot(alphas, scores)
+plt.title('5 fold cross validation accuracy for differing alphas')
+plt.xlabel('Alpha')
+plt.ylabel('Accuracy')
+
+best_alpha = alphas[np.argmax(scores)]
+X, y = get_features(df, best_alpha)
+X_train = X[:-1000]
+X_test = X[-1000:]
+y_train = y[:-1000]
+y_test = y[-1000:]
+m = LogisticRegression(penalty='none').fit(X_train, y_train)
+m.score(X_test, y_test)
